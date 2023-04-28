87e1750cec8cc6cfc2cac532261e45107af0b873
unknown <clayaldas@gmail.com>
2023-04-27 20:03:55 -0500

    Fin de clase jueves 27 de abril

--- app/src/main/java/uta/fisei/app_android_002/ListNumbersActivity.java
+++ app/src/main/java/uta/fisei/app_android_002/ListNumbersActivity.java
@@ -2,13 +2,66 @@ package uta.fisei.app_android_002;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ListNumbersActivity extends AppCompatActivity {
+
+    private ListView listViewData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_numbers);
+
+        listViewData = findViewById(R.id.listViewData);
+
+        // cargar datos en el listView
+        ArrayAdapter<String> adapterNumbers =
+                new ArrayAdapter<String>(this,
+            android.R.layout.simple_list_item_1, loadData());
+
+        listViewData.setAdapter(adapterNumbers);
+
+        listViewData.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String selectedItem = (String) listViewData.getAdapter().getItem(i);
+
+               // Toast.makeText(ListNumbersActivity.this,
+                 //       "Selecciono: " + selectedItem,
+                   //     Toast.LENGTH_SHORT).show();
+
+                // regresar el (selectedItem) hacia el activity padre
+                Intent intent = new Intent();
+                intent.setData(Uri.parse(selectedItem));
+
+                // indicar al compilador que el activity se cierra de forma normal
+                setResult(Activity.RESULT_OK, intent);
+
+                finish();
+            }
+        });
+    }
+
+    private List<String>  loadData() {
+        List<String> list = new ArrayList<String>();
+
+        for (int i=1; i <= 10; i++) {
+            String item = "Numero: " + i;
+
+            list.add(item);
+        }
+        return list;
     }
 }
--- app/src/main/java/uta/fisei/app_android_002/MainActivity.java
+++ app/src/main/java/uta/fisei/app_android_002/MainActivity.java
@@ -1,12 +1,21 @@
 package uta.fisei.app_android_002;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private TextView textViewTitle;
+    private Button buttonFinish;
+
+    private int requiredCode = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textViewTitle = findViewById(R.id.textViewTitle);
+        buttonFinish = findViewById(R.id.buttonFinish);
 
         // recibir / obtener los parametros del activity (login)
         Bundle bundle  = this.getIntent().getExtras();
@@ -22,5 +32,47 @@ public class MainActivity extends AppCompatActivity {
         String password = bundle.getString("keyPassword");
         textViewTitle.setText("Usuario: " + userName +
                 "   " + "Clave:  " + password);
+
+        buttonFinish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+
+    public void onClick_buttonShowBrowser(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW,
+                Uri.parse("http://www.google.com"));
+
+        startActivity(intent);
+    }
+
+    public void onClick_buttonCallPhone (View view) {
+        Intent intent = new Intent(Intent.ACTION_DIAL,
+                Uri.parse("tel: +5934743749"));
+        startActivity(intent);
+    }
+
+    public void onClick_buttonShowListNumber (View view) {
+        Intent intent = new Intent(this,
+                ListNumbersActivity.class);
+
+        //startActivity(intent);
+        startActivityForResult(intent,requiredCode);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+       super.onActivityResult(requestCode, resultCode, data);
+
+        if ( (requestCode == requiredCode) && (resultCode == RESULT_OK)) {
+            textViewTitle.setText("Selecciono: " + data.getDataString());
+        }
+        else {
+            Toast.makeText(this,
+                    "Error al recuperar los datos",
+                    Toast.LENGTH_SHORT).show();
+        }
     }
 }
