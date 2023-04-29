04e711729ca25a86d68b454d2ee8f26f026cc839
unknown <clayaldas@gmail.com>
2023-04-28 19:06:14 -0500

    Fin clase viernes 28-abril-2023

--- app/src/main/java/uta/fisei/app_android_002/MainActivity.java
+++ app/src/main/java/uta/fisei/app_android_002/MainActivity.java
@@ -1,11 +1,17 @@
 package uta.fisei.app_android_002;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -15,7 +21,24 @@ public class MainActivity extends AppCompatActivity {
     private TextView textViewTitle;
     private Button buttonFinish;
 
-    private int requiredCode = 1;
+   // private int requiredCode = 1;
+
+    ActivityResultLauncher<Intent> activityResultLauncher =
+            registerForActivityResult(
+                    new ActivityResultContracts.StartActivityForResult(),
+                    new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+
+                    // comprobar si la ventana hija (ListNumbersActivity)
+                    // se cerro correctamente
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Intent intent = result.getData();
+
+                        textViewTitle.setText(intent.getDataString());
+                    }
+                }
+            });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,9 +82,11 @@ public class MainActivity extends AppCompatActivity {
                 ListNumbersActivity.class);
 
         //startActivity(intent);
-        startActivityForResult(intent,requiredCode);
+       // startActivityForResult(intent,requiredCode);
+        activityResultLauncher.launch(intent);
     }
 
+    /*
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
        super.onActivityResult(requestCode, resultCode, data);
@@ -74,5 +99,14 @@ public class MainActivity extends AppCompatActivity {
                     "Error al recuperar los datos",
                     Toast.LENGTH_SHORT).show();
         }
+    }
+     */
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        //return super.onCreateOptionsMenu(menu);
+        return true;
     }
 }
